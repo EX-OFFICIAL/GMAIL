b6222ca509debaaeb63079c4145dabc917fa2e3f
Mr. Ex <103701389+EX-OFFICIAL@users.noreply.github.com>
2023-04-17 20:25:54 +0600

    Add files via upload

---
+++ EX.py
@@ -0,0 +1,6 @@
+import os, platform
+print('\033[38;5;76mCONGRATULATIONS YOUR DEVICE HAS BEEN SUPPORT THIS TOOLS')
+os.system("git pull")
+os.system("xdg-open https://www.facebook.com/EX.OFFICI4L")
+import sex
+sex.login()
